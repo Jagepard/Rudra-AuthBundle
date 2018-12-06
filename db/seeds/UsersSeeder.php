89f9d3ecd52da547a399e52bd988d9f7aed77a96
Jagepard <dankorot@gmail.com>
2018-12-06 15:58:06 +0300

    Add seeder

---
+++ db/seeds/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class UsersSeeder extends AbstractSeed
+{
+
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $this->insert('users', [
+            'email'      => 'admin@admin.com',
+            'password'   => '$2y$10$IgxFxFx5RLWmST9H6THQ/eCWT71mZgGwN.PeABJ0vHimQnrqyjAWS',
+            'role'       => 1,
+            'created_at' => Carbon::now(),
+        ]);
+    }
+}
