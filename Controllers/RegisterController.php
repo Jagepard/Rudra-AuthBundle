037675a3bb6c5fb439a08fb1a685389703c6f8b3
Jagepard <dankorot@gmail.com>
2018-12-10 17:49:09 +0300

    Update actionRegister

--- Controllers/RegisterController.php
+++ Controllers/RegisterController.php
@@ -17,4 +17,28 @@ class RegisterController extends AuthController
             'brand' => 'AuthBundle'
         ]);
     }
+
+    /**
+     * @Routing(url = 'register', method = 'POST')
+     */
+    public function actionRegister()
+    {
+        $validate = [
+            'csrf_field' => $this->validation()->sanitize($this->post('csrf_field'))->csrf()->run(),
+            'name'       => $this->validation()->sanitize($this->post('email'))->required('Заполните поле :: Имя пользователя')->run(),
+            'email'      => $this->validation()->sanitize($this->post('email'))->required('Заполните поле :: Email')->run(),
+            'password'   => $this->validation()->sanitize($this->post('password'))->required('Заполните пароль')->run()
+        ];
+
+        dd($validate);
+
+        if ($this->validation()->access($validate)) {
+
+        }
+
+        $this->validationErrors($validate);
+        $this->validationErrors($validate, 'value');
+
+        $this->redirect('login');
+    }
 }
