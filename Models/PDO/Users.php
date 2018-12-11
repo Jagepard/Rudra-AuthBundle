6d0f24f2eaa8be8725ecbd732f0823b9f905e117
Jagepard <dankorot@gmail.com>
2018-12-11 18:08:52 +0300

    Update Model

--- Models/PDO/Users.php
+++ Models/PDO/Users.php
@@ -6,6 +6,7 @@ use Rudra\Model;
 
 class Users extends Model
 {
+
     public function getUser(string $name)
     {
         $users = $this->db()->prepare('SELECT * FROM users WHERE `email` = :name');
@@ -14,6 +15,27 @@ class Users extends Model
             ':name' => $name,
         ]);
 
-        return $users->fetch(\PDO::FETCH_ASSOC);;
+        return $users->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function create(array $res)
+    {
+        $this->createRow($res);
+        $this->setSession('alert', 'Данные добавлены', 'success');
+    }
+
+    protected function createRow(array $res)
+    {
+        $query = $this->db()->prepare("               
+               INSERT INTO `users` (`name`, `email`, `password`, `role`, `created_at`) 
+               VALUES (:name, :email, :password, :role, :created_at)");
+
+        $query->execute([
+            ':name'       => $res['name'],
+            ':email'      => $res['email'],
+            ':password'   => $res['password'],
+            ':role'       => '2',
+            ':created_at' => date('Y-m-d H:i')
+        ]);
     }
 }
