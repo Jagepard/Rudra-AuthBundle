7f651f662ddbabd2d936162f8cb996131454a266
Jagepard <dankorot@gmail.com>
2018-12-14 12:05:25 +0300

    Update routes

--- Routes/common.php
+++ Routes/common.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
-    'login'       => ['LoginController'],
-    'login::POST' => ['LoginController', 'actionLogin'],
-    'logout'      => ['LoginController', 'actionLogout'],
+    'login'                  => ['LoginController'],
+    'login::POST'            => ['LoginController', 'actionLogin'],
+    'logout'                 => ['LoginController', 'actionLogout'],
 
     'register'       => ['RegisterController'],
     'register::POST' => ['RegisterController', 'actionRegister'],
+
+    'activate/{email}/{md5}' => ['ActivateController', 'actionActivate'],
 ];
