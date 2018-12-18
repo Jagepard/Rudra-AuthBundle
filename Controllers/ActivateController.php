c184d956fb97696acdc314d4a73fb5c8b50fe0fe
Jagepard <dankorot@gmail.com>
2018-12-18 15:19:44 +0300

    Refactored

--- Controllers/ActivateController.php
+++ Controllers/ActivateController.php
@@ -12,11 +12,7 @@ class ActivateController extends AuthController
      */
     public function actionActivate($params)
     {
-        $user = $this->db()->prepare('SELECT * FROM users WHERE `email` = :email');
-        $user->execute([
-            ':email' => $params['email'],
-        ]);
-        $user = $user->fetch(\PDO::FETCH_ASSOC);
+        $user = $this->model()->getUser($params['email']);
 
         if ($user['status'] == '1') {
             $this->setSession('alert', $user['email'] . ' - уже активирован', 'main');
@@ -24,28 +20,12 @@ class ActivateController extends AuthController
         }
 
         if ($params['md5'] == $user['activate']) {
-            $this->updateStatus($user['email']);
+            $this->model()->updateStatus($user['email']);
             $this->setSession('alert', 'Email подтвержден', 'success');
             $this->redirect('login');
         }
 
         $this->setSession('alert', 'Ссылка неверна', 'main');
         $this->redirect('login');
-    }
-
-    /**
-     * @param string $email
-     */
-    protected function updateStatus(string $email)
-    {
-        $query = $this->db()->prepare("
-                UPDATE `users` SET 
-                status = :status
-                WHERE `email` = :email");
-
-        $query->execute([
-            ':status' => '1',
-            ':email'  => $email,
-        ]);
     }
 }
