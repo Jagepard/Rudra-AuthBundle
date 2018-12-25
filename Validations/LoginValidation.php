90ed6e2c6087b284f3860219d3d52df59426ced5
Jagepard <dankorot@gmail.com>
2018-12-25 15:01:49 +0300

    Style

--- Validations/LoginValidation.php
+++ Validations/LoginValidation.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @copyright Copyright (c) 2018, Korotkov Danila
- * @license   http://www.gnu.org/licenses/gpl.html GNU GPL-3.0
- */
 
 namespace App\Auth\Validations;
 
