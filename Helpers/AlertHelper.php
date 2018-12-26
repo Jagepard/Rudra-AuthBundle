f4c55fd7aa15aeee32f3dbf89188e58f7c48625f
Jagepard <dankorot@gmail.com>
2018-12-26 17:39:07 +0300

    Add void hinting

--- Helpers/AlertHelper.php
+++ Helpers/AlertHelper.php
@@ -13,7 +13,7 @@ trait AlertHelper
         }
     }
 
-    protected function alreadyActivated($user)
+    protected function alreadyActivated($user): void
     {
         if ($user->status == '1') {
             $this->setSession('alert', 'Email уже активирован', 'info');
@@ -21,31 +21,31 @@ trait AlertHelper
         }
     }
 
-    protected function wrongLink()
+    protected function wrongLink(): void
     {
         $this->setSession('alert', 'Ссылка неверна', 'error');
         $this->redirect('login');
     }
 
-    protected function succeeded()
+    protected function succeeded(): void
     {
         $this->setSession('alert', 'Email подтвержден', 'success');
         $this->redirect('login');
     }
 
-    protected function errorMessages(string $target = 'login')
+    protected function errorMessages(string $target = 'login'): void
     {
         $this->validationErrors($this->validate);
         $this->redirect($target);
     }
 
-    protected function notAgree()
+    protected function notAgree(): void
     {
         $this->setSession('alert', 'Вы не согласились с правилами ресурса', 'agree');
         $this->redirect('register');
     }
 
-    protected function alreadyExists($user)
+    protected function alreadyExists($user): void
     {
         if ($user) {
             $this->setSession('alert', 'Пользователь с таким Email уже есть', 'unique');
@@ -53,13 +53,13 @@ trait AlertHelper
         }
     }
 
-    protected function emailVerification()
+    protected function emailVerification(): void
     {
         $this->setSession('alert', 'Данные добавлены', 'success');
         $this->setSession('alert', 'Подтвердите почтовый адрес', 'info');
     }
 
-    protected function sendLink()
+    protected function sendLink(): void
     {
         $this->setSession('alert', 'Ссылка отправлена', 'success');
         $this->setSession('alert', 'Перейдите по ссылке', 'info');
